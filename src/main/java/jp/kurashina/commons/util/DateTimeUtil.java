da039c63f4e89eb2a8b96f57bad2fc62cd538e5e
Tomohiro Kaneda <tomokane@dream.email.ne.jp>
2024-11-14 12:26:31 +0900

    202411141226

---
+++ src/main/java/jp/kurashina/commons/util/DateTimeUtil.java
@@ -0,0 +1,31 @@
+package jp.kurashina.commons.util;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
+
+@Component
+public class DateTimeUtil {
+
+    public static ZonedDateTime asiaTokyo(int year, int month, int day, int hour, int minute, int second) {
+        LocalDateTime localDateTime = LocalDateTime.of(year, month, day, hour, minute, second);
+        return localDateTime.atZone(ZoneId.of("Asia/Tokyo"));
+    }
+
+    public static ZonedDateTime asiaTokyo(int year, int month, int day) {
+        return asiaTokyo(year, month, day, 0, 0, 0);
+    }
+
+    public static ZonedDateTime utc(int year, int month, int day, int hour, int minute, int second) {
+        LocalDateTime localDateTime = LocalDateTime.of(year, month, day, hour, minute, second);
+        return localDateTime.atZone(ZoneId.of("UTC"));
+    }
+
+    public static ZonedDateTime utc(int year, int month, int day) {
+        return utc(year, month, day, 0, 0, 0);
+    }
+
+}
