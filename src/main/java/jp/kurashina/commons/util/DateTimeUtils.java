45fbb74006a4b333e89c7ee7ce2ab0901e983f8d
Tomohiro Kaneda <tomokane@dream.email.ne.jp>
2025-05-03 21:23:01 +0900

    202505032123

--- src/main/java/jp/kurashina/commons/util/DateTimeUtils.java
+++ src/main/java/jp/kurashina/commons/util/DateTimeUtils.java
@@ -2,9 +2,13 @@ package jp.kurashina.commons.util;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.chrono.JapaneseDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 @Component
 public class DateTimeUtils {
@@ -25,6 +29,18 @@ public class DateTimeUtils {
 
     public static ZonedDateTime utc(int year, int month, int day) {
         return utc(year, month, day, 0, 0, 0);
+    }
+
+    public static LocalDate convertJapaneseToGregorian(String source) {
+        DateTimeFormatter japaneseFormatter = DateTimeFormatter.ofPattern("GGyy年M月", Locale.JAPAN);
+        java.time.LocalDate gregorianDate = null;
+        try {
+            java.time.temporal.TemporalAccessor temporalAccessor = japaneseFormatter.parse(source);
+            JapaneseDate japaneseDate = JapaneseDate.from(temporalAccessor);
+            gregorianDate = java.time.LocalDate.from(japaneseDate);
+        } catch (Exception ignored) {
+        }
+        return gregorianDate;
     }
 
 }
