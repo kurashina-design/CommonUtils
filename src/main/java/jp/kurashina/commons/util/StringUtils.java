7d9be34cbb042aa29d6b7ddc39f6d99d1127ab0f
Tomohiro Kaneda <tomokane@dream.email.ne.jp>
2025-09-22 10:56:32 +0900

    202509221056

--- src/main/java/jp/kurashina/commons/util/StringUtils.java
+++ src/main/java/jp/kurashina/commons/util/StringUtils.java
@@ -2,6 +2,7 @@ package jp.kurashina.commons.util;
 
 import org.springframework.stereotype.Component;
 
+import java.text.Normalizer;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -39,6 +40,13 @@ public class StringUtils {
 
     public static String strip(String source) {
         return org.apache.commons.lang3.StringUtils.strip(source, " \u3000\t\n\r\f"); // 半角SP、全角SP、タブ、改行、復帰、改ページ
+    }
+
+    public static String normalizeAsciiOnly(String input) {
+        //正規表現で不要な全角文字を除外するフィルタを追加します。
+        if (input == null) return null;
+        String normalized = Normalizer.normalize(input, Normalizer.Form.NFKC);
+        return normalized.replaceAll("[^\\p{ASCII}]", "").trim(); // ASCII以外を除去
     }
 
     public static String htmlString(String source) {
